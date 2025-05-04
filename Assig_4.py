6a84ac459ddde5c2c3f6183d93ba7e409e4b58c1
AdityaYadav39 <147309151+AdityaYadav39@users.noreply.github.com>
2025-05-04 23:54:13 +0530

    Update Assig_4.py

--- Assig_4.py
+++ Assig_4.py
@@ -1,144 +1,56 @@
-# Python3 program imitating a clock server
-from dateutil import parser
-import threading
-import datetime
-import socket
-import time
+class Node:
+    def __init__(self, name, clock_time):
+        self.name = name
+        self.clock_time = clock_time  # in seconds
 
+    def get_time(self):
+        return self.clock_time
 
-# datastructure used to store client address and clock data
-client_data = {}
+    def adjust_time(self, offset):
+        self.clock_time += offset
 
+class BerkeleyAlgorithm:
+    def __init__(self, nodes):
+        self.nodes = nodes
+        self.master = nodes[0]  # first node is master
 
-''' nested thread function used to receive
-	clock time from a connected client '''
-def startReceivingClockTime(connector, address):
+    def synchronize(self):
+        print("Master Node:", self.master.name)
+        
+        # Step 1: Master polls all nodes
+        time_diffs = []
+        total_time = 0
+        print("\nPolling all nodes for current time:")
+        for node in self.nodes:
+            time = node.get_time()
+            print(f"{node.name} time: {time}")
+            total_time += time
+            time_diffs.append((node, time))
 
-	while True:
-		# receive clock time
-		clock_time_string = connector.recv(1024).decode()
-		clock_time = parser.parse(clock_time_string)
-		clock_time_diff = datetime.datetime.now() - \
-												clock_time
+        # Step 2: Compute average time
+        avg_time = total_time / len(self.nodes)
+        print(f"\nAverage time: {avg_time:.2f}")
 
-		client_data[address] = {
-					"clock_time"	 : clock_time,
-					"time_difference" : clock_time_diff,
-					"connector"	 : connector
-					}
+        # Step 3: Compute offset for each node and send adjustment
+        print("\nAdjusting clocks:")
+        for node, time in time_diffs:
+            offset = avg_time - time
+            print(f"{node.name} offset: {offset:+.2f}")
+            node.adjust_time(offset)
 
-		print("Client Data updated with: "+ str(address),
-											end = "\n\n")
-		time.sleep(5)
+        print("\nClocks after synchronization:")
+        for node in self.nodes:
+            print(f"{node.name} time: {node.get_time():.2f}")
 
+# Example Usage
+if __name__ == "__main__":
+    nodes = [
+        Node("Node1", 100.0),
+        Node("Node2", 98.5),
+        Node("Node3", 102.3),
+        Node("Node4", 97.0),
+    ]
 
-''' master thread function used to open portal for
-	accepting clients over given port '''
-def startConnecting(master_server):
-	
-	# fetch clock time at slaves / clients
-	while True:
-		# accepting a client / slave clock client
-		master_slave_connector, addr = master_server.accept()
-		slave_address = str(addr[0]) + ":" + str(addr[1])
+    berkeley = BerkeleyAlgorithm(nodes)
+    berkeley.synchronize()
 
-		print(slave_address + " got connected successfully")
-
-		current_thread = threading.Thread(
-						target = startReceivingClockTime,
-						args = (master_slave_connector,
-										slave_address, ))
-		current_thread.start()
-
-
-# subroutine function used to fetch average clock difference
-def getAverageClockDiff():
-
-
-	time_difference_list = list(client['time_difference']
-								for client_addr, client
-									in client_data.items())
-									
-
-	sum_of_clock_difference = sum(time_difference_list, \
-								datetime.timedelta(0, 0))
-
-	average_clock_difference = sum_of_clock_difference \
-										/ len(client_data)
-
-	return average_clock_difference
-
-
-''' master sync thread function used to generate
-	cycles of clock synchronization in the network '''
-def synchronizeAllClocks():
-
-	while True:
-
-		print("New synchronization cycle started.")
-		print("Number of clients to be synchronized: " + \
-									str(len(client_data)))
-
-		if len(client_data) > 0:
-
-			average_clock_difference = getAverageClockDiff()
-
-			for client_addr, client in client_data.items():
-				try:
-					synchronized_time = \
-						datetime.datetime.now() + \
-									average_clock_difference
-
-					client['connector'].send(str(
-							synchronized_time).encode())
-
-				except Exception as e:
-					print("Something went wrong while " + \
-						"sending synchronized time " + \
-						"through " + str(client_addr))
-
-		else :
-			print("No client data." + \
-						" Synchronization not applicable.")
-
-		print("\n\n")
-
-		time.sleep(5)
-
-
-# function used to initiate the Clock Server / Master Node
-def initiateClockServer(port = 8080):
-
-	master_server = socket.socket()
-	master_server.setsockopt(socket.SOL_SOCKET,
-								socket.SO_REUSEADDR, 1)
-
-	print("Socket at master node created successfully\n")
-	
-	master_server.bind(('', port))
-
-	# Start listening to requests
-	master_server.listen(10)
-	print("Clock server started...\n")
-
-	# start making connections
-	print("Starting to make connections...\n")
-	master_thread = threading.Thread(
-						target = startConnecting,
-						args = (master_server, ))
-	master_thread.start()
-
-	# start synchronization
-	print("Starting synchronization parallelly...\n")
-	sync_thread = threading.Thread(
-						target = synchronizeAllClocks,
-						args = ())
-	sync_thread.start()
-
-
-
-# Driver function
-if __name__ == '__main__':
-
-	# Trigger the Clock Server
-	initiateClockServer(port = 8080)
